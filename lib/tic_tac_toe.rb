ca03fbccf437c04b1f05f16a8d29be6c3ecc4762
Ryan Brady <ryanbrady369@gmail.com>
2018-10-12 21:46:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -128,7 +128,7 @@ class TicTacToe
       turn
     end
     if won?.is_a?(Array)
-      puts "Congratulations #{winner(board)}!"
+      puts "Congratulations #{winner(@board)}!"
     elsif draw?
       puts "Cat's Game!"
     else
